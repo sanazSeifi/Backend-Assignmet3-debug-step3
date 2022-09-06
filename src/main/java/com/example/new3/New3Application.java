d7ced0668a496498ab804433efa91d6ac297140b
Sanaz <snz5seifi@gmail.com>
2022-09-06 14:59:52 -0400

    MongoDB

---
+++ src/main/java/com/example/new3/New3Application.java
@@ -0,0 +1,13 @@
+package com.example.new3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(New3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new3/New3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class New3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
